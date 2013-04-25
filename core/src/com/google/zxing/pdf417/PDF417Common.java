c33563d5b678bcf55fa0a3002a4a75025a72e5d3
Guenther Grau <zxing.org@grau.net>
2013-04-25 22:02:07 +0200

    add copyright and author

--- core/src/com/google/zxing/pdf417/PDF417Common.java
+++ core/src/com/google/zxing/pdf417/PDF417Common.java
@@ -1,3 +1,18 @@
+/*
+ * Copyright 2009 ZXing authors
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.google.zxing.pdf417;
 
 public class PDF417Common {
@@ -12,6 +27,10 @@ public class PDF417Common {
   public static final int MODULES_IN_STOP_PATTERN = 18;
   public static final int BARS_IN_MODULE = 8;
 
+  /**
+   * @author SITA Lab (kevin.osullivan@sita.aero)
+   * @author Guenther Grau
+   */
   public static int getBitCountSum(int[] moduleBitCount) {
     int bitCountSum = 0;
     for (int count : moduleBitCount) {
